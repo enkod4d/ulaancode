6670e8c6b27dda820175d23506ded7be7be4855c
enkod4d <enkod4d@gmail.com>
2025-04-18 02:58:50 +0800

    base
    base

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect } from "react";
+
 import Layout from "@/components/layout/Layout";
 import Section1 from "@/components/sections/home4/section1";
 import Section10 from "@/components/sections/home4/section10";
@@ -9,7 +12,24 @@ import Section6 from "@/components/sections/home4/section6";
 import Section7 from "@/components/sections/home4/section7";
 import Section8 from "@/components/sections/home4/section8";
 import Section9 from "@/components/sections/home4/section9";
+import Preloader from "./loading";
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading time of 1 or 2 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500); // 2000ms = 2 seconds
+
+    // Cleanup the timer if the component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    // You can replace this with your custom loading indicator
+    return Preloader();
+  }
   return (
     <>
       <Layout headerStyle={4} footerStyle={4}>
