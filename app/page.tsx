9e0690e737a6bd70b12cda43039cb19ea73be8b3
enkod4d <enkod4d@gmail.com>
2025-04-18 01:27:16 +0800

    added public assets
    added public assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Layout from "@/components/layout/Layout";
-import Popup from "@/components/layout/Popup";
-import Section1 from "@/components/sections/home1/section1";
-import Section2 from "@/components/sections/home1/section2";
-import Section3 from "@/components/sections/home1/section3";
-import Section4 from "@/components/sections/home1/section4";
-import Section5 from "@/components/sections/home1/section5";
-import Section6 from "@/components/sections/home1/section6";
-import Section7 from "@/components/sections/home1/section7";
-import Section8 from "@/components/sections/home1/section8";
-import Section9 from "@/components/sections/home1/section9";
+import Section1 from "@/components/sections/home4/section1";
+import Section10 from "@/components/sections/home4/section10";
+import Section2 from "@/components/sections/home4/section2";
+import Section3 from "@/components/sections/home4/section3";
+import Section4 from "@/components/sections/home4/section4";
+import Section5 from "@/components/sections/home4/section5";
+import Section6 from "@/components/sections/home4/section6";
+import Section7 from "@/components/sections/home4/section7";
+import Section8 from "@/components/sections/home4/section8";
+import Section9 from "@/components/sections/home4/section9";
 export default function Home() {
   return (
     <>
-      <Layout headerStyle={1} footerStyle={1}>
+      <Layout headerStyle={4} footerStyle={4}>
         <Section1 />
         <Section2 />
         <Section3 />
@@ -22,9 +22,8 @@ export default function Home() {
         <Section7 />
         <Section8 />
         <Section9 />
+        <Section10 />
       </Layout>
-
-      {/* <Popup /> */}
     </>
   );
 }
